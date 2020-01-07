55278fc2f8a4fc6f7112e3879e28d22f12094d66
sl1673495 <949589855@qq.com>
2020-01-07 23:31:02 +0800

    feat: add function-property-names

--- src/union-to-intersection.ts
+++ src/union-to-intersection.ts
@@ -2,10 +2,7 @@ type MergeParameters<U> = (U extends any
 ? (k: U) => void
 : never) extends (k: infer I) => void
   ? I
-  : never;
+  : never
 
-type F = { a: 1 } | { b: 2 };
-let f = (a: F) => 1
-type P = Parameters<typeof f>;
-
-type Merged = MergeParameters<F>;
+type F = {a: 1} | {b: 2}
+type Merged = MergeParameters<F>
