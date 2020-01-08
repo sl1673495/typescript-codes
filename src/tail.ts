0e0e9b096697850e60c178d173dfe439f1cc0cb5
sl1673495 <949589855@qq.com>
2020-01-09 01:39:35 +0800

    feat: add tail.ts

---
+++ src/tail.ts
@@ -0,0 +1,24 @@
+// ['1','2','3'] -> ['2','3']
+type Tail<Tuple extends any[]> = ((...args: Tuple) => void) extends (
+  a: any,
+  ...args: infer T
+) => void
+  ? T
+  : never
+// ['1','2','3'] -> '1'
+type Head<Tuple extends any[]> = Tuple extends [infer Result, ...any[]]
+  ? Result
+  : never
+// (['2','3'],'1') -> ['1','2','3']
+type Unshift<Tuple extends any[], Element> = ((
+  a: Element,
+  ...args: Tuple
+) => void) extends (...args: infer T) => void
+  ? T
+  : never
+
+type T1 = Tail<[1, 2, 3]>
+
+type T2 = Head<[1, 2, 3]>
+
+type T3 = Unshift<['2', '3'], '1'>
--- src/union-to-intersection.ts
+++ src/union-to-intersection.ts
@@ -4,5 +4,23 @@ type MergeParameters<U> = (U extends any
   ? I
   : never
 
-type F = {a: 1} | {b: 2}
-type Merged = MergeParameters<F>
+
+// 应用在mixin上
+type Ctor<T> = {new (...args: any[]): T}
+declare function mixin<T extends Ctor<any>[]>(
+  ...traits: T
+): Ctor<UnionToIntersection<InstanceType<T[number]>>>
+
+class Flyable {
+  fly() {}
+}
+class Walkable {
+  walk() {}
+}
+
+class Mixed extends mixin(Flyable, Walkable) {
+  test() {
+    this.fly() // ok
+    this.walk() // ok
+  }
+}
