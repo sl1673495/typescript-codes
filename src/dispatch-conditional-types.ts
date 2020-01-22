d140e7c637fed2cb12062dcdb9c8993aeb08946a
ssh <949589855@qq.com>
2020-01-22 21:15:56 +0800

    feat: distribute conditional type

--- src/dispatch-conditional-types.ts
+++ src/dispatch-conditional-types.ts
@@ -1,26 +1,40 @@
-type ActionsForDispatch =
+type Action =
   | {
-      type: 'INIT';
+      type: "INIT"
     }
   | {
-      type: 'SYNC';
+      type: "SYNC"
     }
   | {
-      type: 'LOG_IN';
-      emailAddress: string;
+      type: "LOG_IN"
+      emailAddress: string
     }
   | {
-      type: 'LOG_IN_SUCCESS';
-      accessToken: string;
-    };
+      type: "LOG_IN_SUCCESS"
+      accessToken: string
+    }
 
-declare function dispatch(action: Action): void;
+// 用类型查询查出Action中所有type的联合类型
+type ActionType = Action["type"]
 
-dispatch({
-  type: 'INIT',
-});
+// 把传入的联合类型中"type"类型去除掉
+type ExcludeTypeKey<K> = K extends "type" ? never : K
 
-dispatch({
-  type: 'LOG_IN',
-  emailAddress: '123',
-});
+// 把类型中key为"type"去掉
+type ExcludeTypeField<A> = {
+  [K in ExcludeTypeKey<keyof A>]: A[K]
+}
+
+type ExtractActionParameters<A, T> = A extends { type: T }
+  // 这里也可以用Omit<A, "type">
+  ? ExcludeTypeField<A>
+  : never
+
+declare function dispatch<T extends ActionType>(
+  type: T,
+  args: ExtractActionParameters<Action, T>,
+): void
+
+dispatch("LOG_IN", {
+  emailAddress: "123",
+})
