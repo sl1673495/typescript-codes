82a7f6e1362466ab10ab120f9a2ac4d8696d6bcf
sl1673495 <949589855@qq.com>
2020-07-28 16:21:12 +0800

    feat: exhaustiveness-checking

---
+++ src/exhaustiveness-checking.ts
@@ -0,0 +1,30 @@
+interface Square {
+  kind: "square";
+  size: number;
+}
+interface Rectangle {
+  kind: "rectangle";
+  width: number;
+  height: number;
+}
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+
+type Shape = Square | Rectangle | Circle;
+
+function assertNever(x: never): never {
+  throw new Error("Unexpected object: " + x);
+}
+
+function area(s: Shape) {
+  switch (s.kind) {
+    case "square":
+      return s.size * s.size;
+    case "rectangle":
+      return s.height * s.width;
+    default:
+      return assertNever(s); // error here if there are missing cases
+  }
+}
