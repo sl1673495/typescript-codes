f99e5ffc8a00fc7bc3173057d94606d36f390a66
sl1673495 <949589855@qq.com>
2020-09-04 03:33:31 +0800

    update: 4.1 vuex

--- src/feat-4.1/vuex.ts
+++ src/feat-4.1/vuex.ts
@@ -1,23 +1,53 @@
-type VuexOptions<M, N> = {
-  namespace?: N,
-  mutations: M,
+type GetMutations<Module> = Module extends { mutations: infer M } ? M : never
+
+type AddPrefix<Prefix, Keys> = `${Prefix & string}/${Keys & string}`
+
+type GetModuleMutationKeys<Module, Key> = AddPrefix<Key, keyof GetMutations<Module>> | (Module extends { modules: infer SubModules } ? AddPrefix<Key, GetModulesMutationKeys<SubModules>> : never)
+
+type GetModulesMutationKeys<N> = N extends {} ? {
+   [K in keyof N]: GetModuleMutationKeys<N[K], K>
+}[keyof N] : never
+
+type Action<Mutations, Modules> = keyof Mutations | GetModulesMutationKeys<Modules>
+
+type Store<Mutations, Modules> = {
+   dispatch(action: Action<Mutations, Modules>): void
 }
 
-type Action<M, N> = N extends string ? `${N}/${keyof M & string}` : keyof M
-
-type Store<M, N> = {
-  dispatch(action: Action<M, N>): void
+type VuexOptions<Mutations, Modules> = {
+   mutations: Mutations,
+   modules: Modules
 }
 
-declare function Vuex<M, N>(options: VuexOptions<M, N>): Store<M, N>
+declare function Vuex<Mutations, Modules>(options: VuexOptions<Mutations, Modules>): Store<Mutations, Modules>
 
 const store = Vuex({
-  namespace: "cart" as const,
-  mutations: {
-     add() { },
-     remove() { }
-  }
+   mutations: {
+      root() { },
+   },
+   modules: {
+      cart: {
+         mutations: {
+            add() { },
+            remove() { }
+         }
+      },
+      user: {
+         mutations: {
+            login() { }
+         },
+         modules: {
+            admin: {
+               mutations: {
+                  login() { }
+               },
+            }
+         }
+      }
+   }
 })
 
+store.dispatch("root")
 store.dispatch("cart/add")
-store.dispatch("cart/remove")
+store.dispatch("user/login")
+store.dispatch("user/admin/login")
